dda5ffe680f701c7c546636f8831b6899a68d86b
julibug <72342559+julibug@users.noreply.github.com>
2024-10-24 10:25:21 +0200

    kazoo

--- Assets/AudioManager.cs
+++ Assets/AudioManager.cs
@@ -7,4 +7,9 @@ public class AudioManager : MonoBehaviour
     [SerializeField] AudioSource musicSource;
     public AudioClip musicClip;
 
+    private void Start()
+    {
+        musicSource.clip = musicClip;
+        musicSource.Play();
+    }
 }
